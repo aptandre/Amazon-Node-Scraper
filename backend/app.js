d57a63d5ffca51a3b9ab226fc2c3d3160d07e3f1
andre <andre.alves@ccc.ufcg.edu.br>
2024-02-23 23:22:08 -0300

    Adding other styles for components

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+import express from 'express';
+import { get } from 'axios';
+import cheerio, { load } from 'cheerio';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 function scrapeData(html) {
-    const $ = cheerio.load(html);
+    const $ = load(html);
     const products_list = [];
 
     cheerio('div[data-component-type="s-search-result"]').each((index, element) => {
@@ -21,6 +22,7 @@ function scrapeData(html) {
     return products_list;
 }
 
+app.use(cors());
 app.get('/amazon-scraper/scrape', async (req, res) => {
 
     const keyword = req.query.keyword;
@@ -28,12 +30,11 @@ app.get('/amazon-scraper/scrape', async (req, res) => {
 
     try {
 
-        const response = await axios.get(url);
+        const response = await get(url);
         const data = await scrapeData(response.data);
         res.json(data);
 
     } catch (error) {
-        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAA")
         console.error(error);
         res.status(500).json({ error: 'Fail: an error occured while trying to stablish connection.' });
     }
